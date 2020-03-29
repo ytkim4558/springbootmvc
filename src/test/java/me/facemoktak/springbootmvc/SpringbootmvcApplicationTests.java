1ca85632b22230239ab429b741371c387150f9a5
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-29 09:39:22 +0900

    init

---
+++ src/test/java/me/facemoktak/springbootmvc/SpringbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.facemoktak.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
