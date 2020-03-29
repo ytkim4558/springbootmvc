85e5cf76d9d666f0db33fc715afab18d0de944e8
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-29 13:50:49 +0900

    thymeleaf 예제

---
+++ src/test/java/me/facemoktak/springbootmvc/SampleControllerTest.java
@@ -0,0 +1,39 @@
+package me.facemoktak.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(SampleController.class)
+class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        // 요청 "/hello"
+        // 응답
+        // - 모델 name : yongtak
+        // - 뷰 이름 : hello
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("yongtak")))
+                .andExpect(content().string(containsString("yongtak")));
+
+    }
+}
