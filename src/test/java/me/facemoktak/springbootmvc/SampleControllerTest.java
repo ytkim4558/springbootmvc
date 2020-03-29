b24ae303602307a07444f4b2aaa97b8a25f71a69
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-29 17:24:50 +0900

    HtmlUnit 예제

--- src/test/java/me/facemoktak/springbootmvc/SampleControllerTest.java
+++ src/test/java/me/facemoktak/springbootmvc/SampleControllerTest.java
@@ -1,39 +1,27 @@
 package me.facemoktak.springbootmvc;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(SampleController.class)
 class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
     public void hello() throws Exception {
-        // 요청 "/hello"
-        // 응답
-        // - 모델 name : yongtak
-        // - 뷰 이름 : hello
-
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name", is("yongtak")))
-                .andExpect(content().string(containsString("yongtak")));
-
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("yongtak");
     }
 }
